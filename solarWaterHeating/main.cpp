d62b6602db12d0b3ed685bb1ebf16cfa664f57f0
richardyantas <richardyantas5@gmail.com>
2020-01-21 15:58:37 +0100

    images added

--- solarWaterHeating/main.cpp
+++ solarWaterHeating/main.cpp
@@ -101,14 +101,12 @@ int main()
 
     
     cout << 5*sign( Interval(0.3) - Interval(0.5) ) << endl;
-    Function m1 = Function(x, Return( 0.005*x[0], 0.001*sign(0.001) ));
-    Function m2 = Function(x, Return( -0.005*x[0] + Interval(0.001), 0.001*sign(0.001) ));
-    Function m3 = Function(x, Return( +0.005*x[0], 0.001*sign(-0.001) ));
-    Function m4 = Function(x, Return( -0.005*x[0], 0.001*sign(-0.001) ));
-    //Function m5 = Function(x, Return( +0.005*x[0]+(sign(x[0]-1)+1)/2, 0.001*sign(0.001) ));
-    Function m5 = Function(x, Return( sign( int(x[0]) ), 0.001*sign(0.001) ));
-    Function m6 = Function(x, Return( -0.005*x[0], 0.001*sign(0.001) ));
-    
+    Function m1 = Function(x, Return( 0.005*x[0],0.001*sign(0.001) ));
+    Function m2 = Function(x, Return( -0.005*x[0],0.001*sign(0.001) ));
+    Function m3 = Function(x, Return( +0.005*x[0],0.001*sign(-0.001) ));
+    Function m4 = Function(x, Return( -0.005*x[0],0.001*sign(-0.001) ));
+    Function m5 = Function(x, Return( +0.005*x[0],0.001*sign(0.001) ));
+    Function m6 = Function(x, Return( -0.005*x[0],0.001*sign(0.001) ));
 
 
     vector< vector<double> > Temperature(2);
